f39d0daf23aebd930c97834b65b70ad71ee9fdd8
DESKTOP-C61PO9C\Randomality <brownron199@yahoo.com>
2021-11-04 01:27:33 -0400

    Installed the latest .Net dev pack, solved Intelisense not working, too annoyed to continue right now

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -8,11 +8,14 @@ public class Driver : MonoBehaviour
     void Start()
     {
         
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,0,.01f);
+        transform.Translate(0,0.01f,0);
     }
 }

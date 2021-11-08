69af3a65e704a4f54b707ccb543ea7d771805229
DESKTOP-C61PO9C\Randomality <brownron199@yahoo.com>
2021-11-07 20:38:46 -0500

    Time.delta time added cause dude said so..

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
-{
+{   [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        
         
         
     }
@@ -15,7 +15,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,.01f);
-        transform.Translate(0,0.01f,0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0,0,-steerAmount);
+        transform.Translate(0,moveAmount,0);
     }
 }

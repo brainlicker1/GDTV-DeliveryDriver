3563bfd2e01d1b11fa098d1fa9110943040b76b1
DESKTOP-C61PO9C\Randomality <brownron199@yahoo.com>
2021-11-14 21:20:44 -0500

    Finished the delivery driver MVP, yay, now to do snowboarder

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {   [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
+
+    [SerializeField] float slowSpeed = 20f;
+    [SerializeField] float boostSpeed = 30f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
@@ -19,5 +18,26 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+
+         
+
+        
     }
+    void OnCollisionEnter2D(Collision2D other) {
+        
+        moveSpeed = slowSpeed;
+
+    }
+     void OnTriggerEnter2D(Collider2D other) {
+            
+             if( other.tag == "boost"){
+                 moveSpeed = boostSpeed;
+                 Debug.Log("Boost");
+             }
+             if(other.tag == "break"){
+                 moveSpeed = slowSpeed;
+                 Debug.Log("Break");
+
+             }
+             }
 }
